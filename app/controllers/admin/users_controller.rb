97f568fd51e54fa7cf5cb49ac77a98520d74f623
Alex <avlonitis@msn.com>
2020-11-29 14:15:56 +0000

    Add edit action for user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,17 @@
 module Admin
   class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :show, :update]
+
     def index
       @users = User.page(params[:page]).order(id: :desc)
     end
 
-    def show
-      @user = User.find(params[:id])
+    def update
+      if @user.update(user_params)
+        redirect_to admin_user_path(@user), notice: "Updated"
+      else
+        redirect_to admin_user_path(@user), alert: @user.errors.full_messages
+      end
     end
 
     def destroy
@@ -17,6 +23,22 @@ module Admin
     end
 
     private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(
+        :username,
+        :email,
+        user_detail_attributes: [
+          :gender,
+          :state,
+          :age
+        ]
+      )
+    end
 
     def add_user_to_block_list(user)
       BlockedEmail.create(email: user.email)
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ApplicationRecord
   has_many :oauth_access_tokens,
            foreign_key: :resource_owner_id,
            dependent: :delete_all
+
+  accepts_nested_attributes_for :user_detail
 end
